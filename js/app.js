bbd053e9685666bd151d627d843442c0f7a44631
Romakha10 <makharadzer1@gmail.com>
2022-10-27 16:43:22 -0700

    javascript2

---
+++ js/app.js
@@ -0,0 +1,60 @@
+// დავალება N2
+
+let myNumber = [27, 18, 22, 10, 45];
+
+let average = myNumber.length;
+let number =
+  myNumber[0] + myNumber[1] + myNumber[2] + myNumber[3] + myNumber[4];
+let result = average / number;
+
+console.log(result);
+
+// დავალება N2
+
+let people1 = {
+  Name: "Dimitri",
+  age: 55,
+};
+
+let people2 = {
+  Name: "Mzevinari",
+  age: 50,
+};
+
+let people3 = {
+  Name: "Tazo",
+  age: 23,
+};
+
+let people4 = {
+  Name: "Mariami",
+  age: 14,
+};
+
+let people5 = {
+  Name: "Roma",
+  age: 26,
+};
+
+let peoplePropert = [people1, people2, people3, people4, people5];
+
+console.log(peoplePropert);
+
+// დავალება N3
+
+let information = {
+  firstName: "Roma",
+  lastName: "Makharadze",
+  age: 26,
+  phoneNumbers: 558454631,
+  address: "Kobuleti, Chakvi",
+  zipCode: 6200,
+};
+
+console.log(information);
+
+// დავალება N4
+
+let myInfo = `User name is ${information.firstName}. user age is ${information.age}.user address is ${information.address}.`;
+
+console.log(myInfo);
